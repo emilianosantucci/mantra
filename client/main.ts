77a1101d65704014003c79728b2b18a46746738d
Emiliano Santucci <emiliano.santucci@gmail.com>
2016-08-22 14:35:12 +0200

    Add Angular2 support

---
+++ client/main.ts
@@ -0,0 +1,21 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Observable } from 'rxjs/Rx';
+// import { AsyncPipe } from '@angular/common';
+// import { PolymerElement } from '@vaadin/angular2-polymer';
+//
+// @Component({
+//     selector: 'ft-main',
+//     // FIXME: import template when TypeScript 2.0 is released
+//     template: require('./main.component.html').default,
+//     directives: [ ],
+//     pipes: [ AsyncPipe ]
+// })
+// export class Main implements OnInit {
+//     $: Observable<>;
+//
+//     constructor(private _service: ) {
+//         this.$ = _service.;
+//     }
+//
+//     ngOnInit() { }
+// }
